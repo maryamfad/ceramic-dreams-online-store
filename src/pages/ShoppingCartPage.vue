023ca40b69a1169df57ebe6e4305a7b111369df0
maryamfadaee@gmail.com <maryamfadaee@gmail.com>
2023-05-19 18:58:33 -0400

    modify the shopping cart page

--- src/pages/ShoppingCartPage.vue
+++ src/pages/ShoppingCartPage.vue
@@ -1,9 +1,27 @@
 <template>
-    <h1>Shopping Cart Page</h1>
+  <h1>Shopping Cart</h1>
+
+  <div class="product-container" v-for="product in cartItems" :key="product.id">
+    <img class="product-image" :src="product.imageName" />
+    <div class="details-wrap">
+      <h3>{{ product.name }}</h3>
+      <p>{{ product.price }}</p>
+    </div>
+    <button class="remove-button">Remove from Cart</button>
+  </div>
+
+  <button class="checkout-button">Proceed to Checkout</button>
 </template>
 
 <script>
+import { cartItems } from "../temp-data";
+
 export default {
-    name: "ShoppingCartPage",
-}
+  name: "ShoppingCartPage",
+  data() {
+    return {
+      cartItems,
+    };
+  },
+};
 </script>
