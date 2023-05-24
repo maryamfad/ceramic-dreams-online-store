e09e7a2073d1fc6dc3178534c476ab2d53be22e4
maryamfadaee@gmail.com <maryamfadaee@gmail.com>
2023-05-24 12:05:47 -0400

    create routes in Express

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,24 @@
 import express from "express";
+import { cartItems, products } from "./temp-data";
 
 const app = express();
 
-app.get('/hello', (req, res)=>{
-    res.send('Hello!')
+app.get('/healthCheck', (req, res)=>{
+    res.send('Healthy!')
+})
+
+app.get('/products', (req, res)=>{
+   res.json(products)
+})
+
+app.get('/cart', (req, res)=>{
+   res.json(cartItems)
+})
+
+app.get('/products/:productId', (req, res)=>{
+    const productId = req.params.productId
+   const product = products.find(p => p.id === productId)
+   res.json(product)
 })
 app.listen(8000, () => {
   console.log("Server is listening on port 8000");
