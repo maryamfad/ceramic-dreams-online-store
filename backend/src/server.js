6279924e8da381204c06a3b1f9f30e872b048af2
maryamfadaee@gmail.com <maryamfadaee@gmail.com>
2023-05-25 13:20:55 -0400

    connect node js to mongodb

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,4 +1,5 @@
 import express from "express";
+import { MongoClient } from "mongodb";
 import {
   cartItems as cartItemsRaw,
   products as productsRaw,
@@ -7,20 +8,31 @@ import {
 let cartItems = cartItemsRaw;
 let products = productsRaw;
 
+require('dotenv').config()
 const app = express();
 
 app.use(express.json());
+
+const dbUrl = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.74roz0k.mongodb.net/?retryWrites=true&w=majority`;
+const dbClient = new MongoClient(dbUrl);
 
 app.get("/healthCheck", (req, res) => {
   res.send("Healthy!");
 });
 
-app.get("/products", (req, res) => {
+app.get("/products", async (req, res) => {
+  await dbClient.connect();
+  const db = dbClient.db("ceramic-dreams-db");
+  const products = await db.collection("products").find({}).toArray();
   res.json(products);
 });
 
+const populatedCartIds = (ids) => {
+  return ids.map((id) => products.find((p) => p.id === id));
+};
 app.get("/cart", (req, res) => {
-  res.json(cartItems);
+  const populatedCart = populatedCartIds(cartItems);
+  res.json(populatedCart);
 });
 
 app.get("/products/:productId", (req, res) => {
@@ -31,15 +43,16 @@ app.get("/products/:productId", (req, res) => {
 
 app.post("/cart", (req, res) => {
   const productId = req.body.id;
-  const product = products.find((p) => p.id === productId);
-  cartItems.push(product);
-  res.json(cartItems);
+  cartItems.push(productId);
+  const populatedCart = populatedCartIds(cartItems);
+  res.json(populatedCart);
 });
 
 app.delete("/cart/:productId", (req, res) => {
   const productId = req.params.productId;
-  const remainingCartItems = cartItems.filter((p) => p.id !== productId);
-  res.json(remainingCartItems);
+  const remainingCartItems = cartItems.filter((id) => id !== productId);
+  const populatedCart = populatedCartIds(remainingCartItems);
+  res.json(populatedCart);
 });
 app.listen(8000, () => {
   console.log("Server is listening on port 8000");
--- backend/src/temp-data.js
+++ backend/src/temp-data.js
@@ -1,80 +1,61 @@
-// import pottery1 from "./assets/1.jpg";
-// import pottery2 from "./assets/2.jpg";
-// import pottery3 from "./assets/3.jpg";
-// import pottery4 from "./assets/4.jpg";
-// import pottery5 from "./assets/5.jpg";
-// import pottery6 from "./assets/6.jpg";
-// import pottery7 from "./assets/7.jpg";
-// import pottery8 from "./assets/8.jpg";
+// import pottery1 from './assets/1.jpg';
+// import pottery2 from './assets/2.jpg';
+// import pottery3 from './assets/3.jpg';
+// import pottery4 from './assets/4.jpg';
+// import pottery5 from './assets/5.jpg';
+// import pottery6 from './assets/6.jpg';
+// import pottery7 from './assets/7.jpg';
+// import pottery8 from './assets/8.jpg';
 
-export const cartItems = [
-  {
-    id: "123",
-    name: "Sun",
-    price: "$26.00",
-    // imageName: pottery1,
-  },
-  {
-    id: "345",
-    name: "Garlic",
-    price: "$26.00",
-    // imageName: pottery3,
-  },
-  {
-    id: "678",                                
-    name: "Moon",
-    price: "$24.50",
-    // imageName: pottery6,
-  },
-];
+export const cartItems = ["123", "456"];
 
 export const products = [
   {
     id: "123",
     name: "Sun",
     price: "$26.00",
-    // imageName: pottery1,
+    imageURL: "/images/pottery1.jpg",
   },
   {
     id: "234",
     name: "Flower",
     price: "$26.00",
-    // imageName: pottery2,
+    imageURL: "/images/pottery2.jpg",
   },
   {
     id: "345",
     name: "Garlic",
     price: "$26.00",
-    // imageName: pottery3,
+    imageURL: "/images/pottery3.jpg",
   },
   {
     id: "456",
     name: "Rosemary",
     price: "$26.00",
-    // imageName: pottery4,
+    imageURL: "/images/pottery4.jpg",
   },
   {
     id: "567",
     name: "Lili",
     price: "$26.00",
-    // imageName: pottery5,
+    imageURL: "/images/pottery5.jpg",
   },
   {
     id: "678",
     name: "Moon",
     price: "$24.50",
-    // imageName: pottery6,
+    imageURL: "/images/pottery6.jpg",
   },
   {
     id: "789",
     name: "Earth",
     price: "$24.50",
-    // imageName: pottery7,
+    imageURL: "/images/pottery7.jpg",
   },
   {
     id: "890",
     name: "Sweat",
     price: "$24.50",
-    // imageName: pottery8,
+    imageURL: "/images/pottery8.jpg",
   },
 ];
