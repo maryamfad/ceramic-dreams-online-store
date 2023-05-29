fe138d860a80eb09cbac705295602c0844443d72
maryamfadaee@gmail.com <maryamfadaee@gmail.com>
2023-05-29 16:29:57 -0400

    show item already is in cart

--- src/pages/ProductDetailPage.vue
+++ src/pages/ProductDetailPage.vue
@@ -5,7 +5,8 @@
         <img :src="product.imageURL" />
         <h1>{{ product.name }}</h1>
         <h3 class="price">{{ product.price }}</h3>
-        <button @click="addToCart" class="add-to-cart">Add to cart</button>
+        <button @click="addToCart" class="add-to-cart" v-if="!isItemInCart">Add to cart</button>
+        <button class="grey-button" v-if="isItemInCart">Item is already in cart!</button>
       </div>
     </div>
   </div>
@@ -15,28 +16,44 @@
 </template>
 
 <script>
-import axios from 'axios';
+import axios from "axios";
 import NotFoundPage from "./NotFoundPage.vue";
 export default {
   name: "ProductDetailPage",
   data() {
     return {
       product: {},
+      cartItems: [],
     };
   },
-  methods:{
-    async addToCart(){
-      await axios.post('/api/users/12345/cart', {id: this.$route.params.productId} )
-      alert('Successfully added item to the cart!')
-    }
+  computed: {
+    isItemInCart() {
+      return this.cartItems.some(
+        (item) => item.id === this.$route.params.productId
+      );
+    },
+  },
+  methods: {
+    async addToCart() {
+      await axios.post("/api/users/12345/cart", {
+        id: this.$route.params.productId,
+      });
+      alert("Successfully added item to the cart!");
+    },
   },
   components: {
     NotFoundPage,
   },
   async created() {
-    const response = await axios.get(`/api/products/${this.$route.params.productId}`);
+    const response = await axios.get(
+      `/api/products/${this.$route.params.productId}`
+    );
     const product = response.data;
     this.product = product;
+
+    const cartResponse = await axios.get("/api/users/12345/cart");
+    const cartItems = cartResponse.data;
+    this.cartItems = cartItems;
   },
 };
 </script>
